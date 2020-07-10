0bae9cbd4df6b8626fc2642a418c4afa420f099d
namanicebed <naman.icebed@gmail.com>
2020-07-10 13:27:24 +0530

    added release version

--- src/actions/home-actions.js
+++ src/actions/home-actions.js
@@ -6,7 +6,7 @@ export const getImages = () => async (dispatch) => {
   try {
     dispatch({type: IMAGE_FETCH_AWAIT});
     const response = await axios.get(
-      'http://jsonplaceholder.typicode.com/photos?_start=0&_limit=200',
+      'https://jsonplaceholder.typicode.com/photos?_start=0&_limit=200',
     );
     dispatch({type: IMAGE_FETCHED, payload: response});
   } catch (error) {
